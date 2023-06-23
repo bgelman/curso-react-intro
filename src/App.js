8d2b23f2c59fb5316b5878bf4b7990e5c6336f70
bgelman@netrails.com.ar <bgelman@netrails.com.ar>
2023-06-23 17:55:13 -0300

    Creación de componentes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import logo from './platzi.webp';
 import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 
 function App() {
-  return (
-    <div className="App">
-      < TodoItem />
-      < TodoItem />
-      < TodoItem />
-      < TodoItem />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+      <React.Fragment>
+
+        <TodoCounter completed={16} total={25} / >
+        <
+        TodoSearch / >
+
+        <
+        TodoList >
+        <
+        TodoItem / >
+        <
+        TodoItem / >
+        <
+        TodoItem / >
+        <
+        /TodoList>
+
+        <
+        CreateTodoButton / >
+
+        <
+        /React.Fragment>
+    );
 }
 
-function TodoItem() {
-  return(
-    <li>
-    <span>V</span>
-    <p>Primer tarea</p>
-    <span>X</span>
-    </li>
-  );
-}
 
-export default App ;
+
+export default App;
---
+++ src/TodoSearch.js
@@ -0,0 +1,5 @@
+function TodoSearch() {
+    return ( < input placeholder = "Cortar ceboollas " > < /input>);
+    }
+
+    export { TodoSearch };
